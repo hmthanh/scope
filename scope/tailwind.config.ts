a9b73cfb46af0c4f5dbc33398af797a7db89523b
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2024-03-27 14:37:27 +0700

    add css

--- scope/tailwind.config.ts
+++ scope/tailwind.config.ts
@@ -1,20 +1,68 @@
 import type { Config } from "tailwindcss";
+const colors = require('tailwindcss/colors')
+
+const makePrimaryColor =
+  (l:any) =>
+  ({ opacityValue }:any) => {
+    return (
+      `hsl(var(--nextra-primary-hue) var(--nextra-primary-saturation) ${l}%` +
+      (opacityValue ? ` / ${opacityValue})` : ')')
+    )
+  }
+
 
 const config: Config = {
+  prefix: 'nx-',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./icons/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
     },
+    fontSize: {
+      xs: '.75rem',
+      sm: '.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem'
+    },
+    letterSpacing: {
+      tight: '-0.015em'
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000',
+      white: '#fff',
+      gray: colors.gray,
+      slate: colors.slate,
+      neutral: colors.neutral,
+      red: colors.red,
+      orange: colors.orange,
+      blue: colors.blue,
+      yellow: colors.yellow,
+    },
+    extend: {
+      colors: {
+        dark: '#111'
+      }
+    }
   },
   plugins: [],
+  darkMode: ['class', 'html[class~="dark"]']
 };
 export default config;
